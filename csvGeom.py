e45816af72f6bc3a4803329b119667d624f1d001
marcel <marcel.singer@stud.hs-bochum.de>
2023-05-21 18:48:20 +0200

    Further split methods. Alsoadded  new dummy-methods for line- and point-creation.

--- csvGeom.py
+++ csvGeom.py
@@ -7,7 +7,10 @@ import io
 DELIMITER = ','
 PROGRAM_TITLE = "csvGeom v0.1.0"
 OUTPUT_FORMAT = "geojson"
-OUTPUT_SUFFIX = "_polygon." + OUTPUT_FORMAT
+OUTPUT_SUFFIX_POLYGON = "_polygon." + OUTPUT_FORMAT
+OUTPUT_SUFFIX_LINE = "_line." + OUTPUT_FORMAT
+OUTPUT_SUFFIX_POINT = "_point." + OUTPUT_FORMAT
+TAB = "    " # Four spaces
 
 def createLayout():
     return [
@@ -26,7 +29,7 @@ def createLayout():
                 ]
             ]
 
-def createGui():
+def initializeGui():
     layout = createLayout()
 
     return sg.Window(PROGRAM_TITLE, layout)
@@ -38,56 +41,67 @@ def createDictionary(inpFileName):
         for row in reader:
             dict_list.append(row)
         return dict_list
+    
+def getFileNameWithoutSuffix(filename):
+    return filename.rsplit(".", 1)[0]
 
 def convertData(values):
     inpFileName = values['-IN-']
-    inpNewFileName = inpFileName.replace('.csv', '')
+    outputFileName = getFileNameWithoutSuffix(inpFileName)
 
     dict_list = createDictionary(inpFileName)
+           
+    data = createPolygon(dict_list)
 
-    dict_len = len(dict_list)
-            
-    file = io.open(str(inpNewFileName + OUTPUT_SUFFIX), "w")
+    writePolygonToFile(outputFileName, data)
 
-    # Zeile für die FeatureCollection, braucht man nur ein mal
-    file.write('{\n"type": "FeatureCollection",\n "name": "csvGeom-Export",\n')
+def createPolygon(dict):
+    data = '{\n"type": "FeatureCollection",\n"name": "csvGeom-Export",\n'
+    data = data +'"features": [ {\n"type": "Feature",\n'
+    data = data +'"properties": {\n"identifier": "'+str(dict[0]['Attribut1'])+'"\n},\n'
+    data = data +'"geometry": { "type": "Polygon",\n'
+    data = data +'"coordinates": \n[\n' + TAB + '[\n'
 
-    # Start eines Features (= ein Befund = ein Polygon, d.h. im späteren loop(?) braucht man das für jeden unerschiedlichen Wert in der Attributspalte)
-    # also muss dann hier eigentlich der (erste) loop (oder was auch immer) anfangen. (das geht bestimmt dynamischer und besser als loopen, aber das ganze war eher improvisiert und nicht notwendigerweise durchdacht)
-    file.write('"features": [ {\n"type": "Feature",\n')
-    # Der "Name" des Befundes als identifier in den properties des einzelnen Features
-    # äh, [0] ist offensichtlich improvisierter müll, weil gerade noch nur eine Datei mit nur einem Befund eingelesen wird.
-    file.write('"properties": {\n"identifier": "'+str(dict_list[0]['Attribut1'])+'"\n},\n')
-    # die Geometrie/die Koordinaten brauchen einen Type, hier: Polygon, später das, was man halt auswählt
-    file.write('"geometry": { "type": "Polygon",\n')
-    # hier dann der anfang vom Polygon
-    file.write('"coordinates":\n [\n[\n')
+    dict_len = len(dict)
 
-    # Zeilen 36-hier kann man dann theoretisch auch in einer Zeile abhandeln, ich persönlich finde das dann aber unverständlich und übersichtlich, also lieber so und dann vermutlich langsamer als völlig undurchsichtig
-
-    # jetz über die einzelnen koordinaten iterieren, jede wird ein punkt im Polygon
     for i in range(dict_len):
-        file.write("        [\n")
-        file.write("            " + str(dict_list[i]['East']).replace(' ', '') + ",\n")
-        file.write("            " + str(dict_list[i]['North']).replace(' ', '') + ",\n")
-        file.write("            " + str(dict_list[i]['Height']).replace(' ', '') + "\n")
-        # wenn wir in der letzten Zeile sind kein Komma
+        data = data + TAB + TAB + "[\n"
+        data = data + TAB + TAB + TAB + str(dict[i]['East']).replace(' ', '') + ",\n"
+        data = data + TAB + TAB + TAB + str(dict[i]['North']).replace(' ', '') + ",\n"
+        data = data + TAB + TAB + TAB + str(dict[i]['Height']).replace(' ', '') + "\n"
         if i+1 == dict_len:
-            file.write("        ]\n")
-        # wenn nicht doch ein Komma
+            data = data + TAB + TAB + "]\n"
         else:
-            file.write("        ],\n")
-    # schließende Klammern für das Polygon
-    file.write("    ]\n]")
+            data = data + TAB + TAB + "],\n"
+    data = data + TAB + "]\n]"
     
-    # schließende Klammern für das JSON-gedönse oben
-    file.write("} }]}")
+    data = data + "} }]}"
 
+    return data
+
+def createLine():
+    pass
+
+def createPoint():
+    pass
+
+def writePolygonToFile(filename, data):
+    writeToFile(filename, data, OUTPUT_SUFFIX_POLYGON)
+
+def writeLineToFile():
+    pass
+
+def writePointToFile():
+    pass
+
+def writeToFile(filename, data, suffix):
+    file = io.open(filename + suffix, "w")
+    file.write(data)
     file.close()
 
 def main():
     
-    window = createGui()
+    window = initializeGui()
 
     while True:
         event, values = window.read()
