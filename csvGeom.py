f0b4d8c405885d15877ed537affebbca80daf680
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-05-22 11:59:58 +0200

    indentation and single point creation as functions
    (the indentation is fluff and not needed for the geojson to work, however: it is a nightmare to read/check it otherwise)

--- csvGeom.py
+++ csvGeom.py
@@ -10,7 +10,6 @@ OUTPUT_FORMAT = ".geojson"
 OUTPUT_SUFFIX_POLYGON = "_polygon" + OUTPUT_FORMAT
 OUTPUT_SUFFIX_LINE = "_line" + OUTPUT_FORMAT
 OUTPUT_SUFFIX_POINT = "_point" + OUTPUT_FORMAT
-TAB = "    " # Four spaces
 
 def createLayout():
     return [
@@ -45,44 +44,75 @@ def createDictionary(inpFileName):
 def getFileNameWithoutSuffix(filename):
     return filename.rsplit(".", 1)[0]
 
+def indent(n):
+    indent = '    ' * n # Four spaces times n
+    indent = '\n' + indent
+    return indent
+
 def convertData(values):
     inpFileName = values['-IN-']
     outputFileName = getFileNameWithoutSuffix(inpFileName)
 
     dict_list = createDictionary(inpFileName)
-           
-    data = createPolygon(dict_list)
+    
+    data = '{'
+    data = data + indent(1) + '"type": "FeatureCollection",' 
+    data = data + indent(1) + '"name": "csvGeom-Export",'
+    data = data + indent(1) + '"features": [ '
+
+    data = createPolygonFeature(dict_list, data)
+
+    # closing bracket for features + Feature Collection
+    data = data + indent(1) + ']' + '\n}'
 
     writePolygonToFile(outputFileName, data)
 
-def createPolygon(dict):
-    data = '{\n"type": "FeatureCollection",\n"name": "csvGeom-Export",\n'
-    data = data +'"features": [ {\n"type": "Feature",\n'
-    data = data +'"properties": {\n"identifier": "'+str(dict[0]['Attribut1'])+'"\n},\n'
-    data = data +'"geometry": { "type": "Polygon",\n'
-    data = data +'"coordinates": \n[\n' + TAB + '[\n'
+# sollte nur 'values' akzeptieren: also schon ein 
+def createPoint(values, minIndent, last):
+    point = indent(minIndent) + '['
+    point = point + indent(minIndent + 1) + values[0] + ','
+    point = point + indent(minIndent + 1) + values[1] + ','
+    point = point + indent(minIndent + 1) + values[2] + ''
+    point = point + indent(minIndent) + ']'
+    if last == False: 
+        point = point + ','
+    
+    return point
+
+def createPolygonFeature(dict, data):
+    data = data + indent(2) + '{'
+    data = data + indent(3) + '"type": "Feature",'
+    data = data + indent(3) + '"properties": {'
+    data = data + indent(4) + '"identifier": "' + str(dict[0]['Attribut1']) + '"'
+    data = data + indent(3) + '},'
+    data = data + indent(3) + '"geometry": {'
+    data = data + indent(4) + '"type": "Polygon",'
+    data = data + indent(4) + '"coordinates": ['
+    data = data + indent(5) + '['
 
     dict_len = len(dict)
 
-    for i in range(dict_len):
-        data = data + TAB + TAB + "[\n"
-        data = data + TAB + TAB + TAB + str(dict[i]['East']).replace(' ', '') + ",\n"
-        data = data + TAB + TAB + TAB + str(dict[i]['North']).replace(' ', '') + ",\n"
-        data = data + TAB + TAB + TAB + str(dict[i]['Height']).replace(' ', '') + "\n"
-        if i+1 == dict_len:
-            data = data + TAB + TAB + "]\n"
-        else:
-            data = data + TAB + TAB + "],\n"
-    data = data + TAB + "]\n]"
     
-    data = data + "} }]}"
+    for i in range(dict_len):
+        values = [str(dict[i]['East']).replace(' ', ''), str(dict[i]['North']).replace(' ', ''), str(dict[i]['Height']).replace(' ', '')]
+        if i+1 == dict_len:
+            data = data + createPoint(values, 6, True)
+        else:
+            data = data + createPoint(values, 6, False)
+
+    data = data + indent(5) + ']'
+    data = data + indent(4) + ']'
+    
+    # closing brackets for geometry + single feature
+    data = data + indent(3) + '}'
+    data = data + indent(2) + '}'
 
     return data
 
-def createLine():
+def createLineFeature():
     pass
 
-def createPoint():
+def createPointFeature(values):
     pass
 
 def writePolygonToFile(filename, data):
