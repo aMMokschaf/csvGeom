fdcb2093b575e84338d458227a3e29b3bf47763f
marcel <marcel.singer@stud.hs-bochum.de>
2023-05-28 18:52:05 +0200

    Refactored event-loop and main-method.

--- csvGeom.py
+++ csvGeom.py
@@ -21,66 +21,59 @@ class Main():
         self.writer = FileWriter()
         self.logger = Logger()
         self.modeller = Modeller()
+        self.inputReader = InputReader()
+
         self.dict = []
         self.filteredDict = []
+
+        self.selectedFileName = None
         self.selectedType = OutputType.POLYGON
         self.selectedFileType = FileType.GEO_JSON
+
+        self.featureCollectionModel = None
 
     def main(self):
 
         gui = Gui(self.PROGRAM_TITLE)
         window = gui.initializeGui()
 
-        inputReader = InputReader()
-        featureCollectionModel = None
-
         while True:
             event, values = window.read()
 
             if event == "-IN-":
-                inputFileName = values['-IN-']
+                self.selectedFileName = values['-IN-']
+                self.logger.info("File chosen: " + self.selectedFileName)
+                self.dict = self.inputReader.createDictionary(self.selectedFileName)
 
-                self.dict = inputReader.createDictionary(inputFileName)
-
-                list = inputReader.createDropDownList(self.dict)
-
+                list = self.inputReader.createDropDownList(self.dict)
                 window["CodeSelected"].update(values=list, disabled=False)
-
-                self.logger.info("File chosen: " + inputFileName)
 
             if event == "CodeSelected":
                 selectedCode = values['CodeSelected']
-                self.filteredDict = inputReader.filterByCode(self.dict, selectedCode)
-
-                window["Convert"].update(disabled=False)
-
+                self.filteredDict = self.inputReader.filterByCode(self.dict, selectedCode)
                 self.logger.info("Code selected: " + selectedCode)
 
-                splitData = inputReader.splitByIdentifier(self.filteredDict)
+                splitData = self.inputReader.splitByIdentifier(self.filteredDict)
 
-                self.logger.info("Found " + str(len(splitData)) + " objects.", splitData)
-
-            if event == "GeomPolygon":
-                self.selectedType = OutputType.POLYGON
-                self.logger.info("Output-type selected: " + self.selectedType.value)
+                window["Convert"].update(disabled=False)
+                self.logger.info(f"Found {str(len(splitData))} objects.", splitData)
 
             if event == "GeomPoint":
                 self.selectedType = OutputType.POINT.value
                 self.logger.info("Output-type selected: " + self.selectedType.value)
 
+            if event == "GeomPolygon":
+                self.selectedType = OutputType.POLYGON
+                self.logger.info("Output-type selected: " + self.selectedType.value)
+
             if event == "Convert":
-
                 featureCollectionModel = self.modeller.convertInputToModel(splitData, self.selectedType)
-
                 self.logger.info("Converted to object-model.")
-
-                inputFileName = values['-IN-']
-                outputFileName = self.util.createOutputFileName(inputFileName, self.selectedType, self.selectedFileType,)
 
                 data = OutputFormatter().createFeatureCollection(featureCollectionModel)
 
+                outputFileName = self.util.createOutputFileName(self.selectedFileName, self.selectedType, self.selectedFileType)
                 self.writer.writeToFile(data, outputFileName)
-
                 self.logger.info("File written: " + outputFileName)
 
             if event == "Close" or event == sg.WIN_CLOSED:
