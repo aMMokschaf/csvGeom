198a6049e3651a0184f70797f1159af058e43a3d
marcel <marcel.singer@stud.hs-bochum.de>
2023-05-28 12:44:11 +0200

    Added temporary method for object-model-debugging in Util-class.

--- util.py
+++ util.py
@@ -10,3 +10,17 @@ class Util():
     def getFileNameWithoutSuffix(self, filename):
         return filename.rsplit(".", 1)[0]
     
+    #temporary method to debug the object-model
+    def debugFeatureCollection(featureCollection):
+        print("fc type: ", featureCollection.type)
+        print("fc name: ", featureCollection.name)
+        print("fc features")
+        for feature in featureCollection.features:
+            print("identifier:", feature.identifier)
+            print("geometry: ")
+            geometry = feature.geometry
+            for coord in geometry.coordinates:
+                print("east: ", coord.east)
+                print("north: ", coord.north)
+                print("height: ", coord.height)
+                print("----")
