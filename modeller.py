65c071fffb2627de9514646e70a7c4f18a9e0e0b
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-06-01 09:47:17 +0200

    notify for invalid geometry

--- modeller.py
+++ modeller.py
@@ -6,10 +6,12 @@ from geojson.coordinate import Coordinate
 
 from enums.outputType import OutputType
 
+from utils.logger import Logger
+
 class Modeller():
 
     def __init__(self):
-        pass
+        self.logger = Logger()
 
     def createCoordinate(self, dictLine):
         east = str(dictLine['East']).replace(' ', '')
@@ -50,10 +52,29 @@ class Modeller():
         return geometry
 
     def createFeature(self, dict, geometryType):
-        geometry = self.createGeometry(dict, geometryType)
-
+        
         identifier = dict[0]['Attribut1']
+        
+        eval = True
 
+        # for now, only notify
+        if geometryType == OutputType.POINT and len(dict) > 1:
+            self.logger.info(f"More than one coordinate for: {str(identifier)}. Using only first coordinate!")
+            dict = dict[0]
+            eval = True
+        if geometryType == OutputType.LINE and len(dict) < 2:
+            self.logger.error(f"Not enough Coordinates for Line: {str(identifier)}")
+            eval = False
+        if geometryType == OutputType.POLYGON and len(dict) < 3:
+            self.logger.error(f"Not enough Coordinates for Polygon: {str(identifier)}")
+            eval = False
+
+        if eval == True:
+            self.logger.info(f"Success:  {str(identifier)}")
+        else:
+            self.logger.error(f"Feature {str(identifier)} has invalid geometry!")
+        
+        geometry = self.createGeometry(dict, geometryType)
         return Feature(identifier, geometry)
 
     def createFeatures(self, dicts, geometryType):
