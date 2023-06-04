27810f6aeca244620799d5814f3e17545beebc21
marcel <marcel.singer@stud.hs-bochum.de>
2023-06-04 14:10:39 +0200

    Added new logging-features for better debugging

--- utils/logger.py
+++ utils/logger.py
@@ -9,25 +9,28 @@ class LogType(Enum):
 
 class Logger():
 
-    DELIMITER = ">>>"
+    INFO_DELIMITER = ">"
+    DEBUG_DELIMITER = "-->"
+    ERROR_DELIMITER = "!"
+    CRITICAL_DELIMITER = "X"
 
     def __init__(self):
         pass
 
     def info(self, msg, objects=[]):
-        self.printMsg(LogType.INFO.value, msg)
+        self.printMsg(LogType.INFO.value, self.INFO_DELIMITER, msg)
         if len(objects) > 0:
             for obj in objects:
                 print(Util().indent(1), str(obj), '\n')
 
     def debug(self, msg):
-        self.printMsg(LogType.DEBUG.value, msg)
+        self.printMsg(LogType.DEBUG.value, self.DEBUG_DELIMITER, msg)
 
     def error(self, msg):
-        self.printMsg(LogType.ERR.value, msg)
+        self.printMsg(LogType.ERR.value, self.ERROR_DELIMITER,msg)
 
     def critical(self, msg):
-        self.printMsg(LogType.CRIT.value, msg)
+        self.printMsg(LogType.CRIT.value, self.CRITICAL_DELIMITER, msg)
     
-    def printMsg(self, logType, msg):
-        print(logType, self.DELIMITER, msg)
+    def printMsg(self, logType, delimiter, msg):
+        print(logType, delimiter, msg)
