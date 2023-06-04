1854583b86f9a6dcdbb80a3e3dfc02d32a15307e
marcel <marcel.singer@stud.hs-bochum.de>
2023-06-04 17:56:57 +0200

    Refactored fileWriter.py

--- utils/fileWriter.py
+++ utils/fileWriter.py
@@ -12,10 +12,12 @@ class FileWriter():
             file = io.open(filename, "w")
             file.write(data)
             file.close()
+
             self.logger.info(f"File succesfully written: {filename}")
+            
         except FileNotFoundError:
             self.logger.error("File not found!")
         except PermissionError:
             self.logger.error("You don't have permission to write this file.")
         except IOError:
-            self.logger.error(f"Error while writing the file {filename}")
+            self.logger.error(f"Error while writing the file {filename}.")
