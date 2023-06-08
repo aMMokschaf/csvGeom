138d599b597a3e84e123bc79613ad7700f778e44
marcel <marcel.singer@stud.hs-bochum.de>
2023-06-08 14:18:10 +0200

    Replaced lists with csvRow-Object.

--- inputReader.py
+++ inputReader.py
@@ -2,6 +2,7 @@ import csv
 import io
 
 from utils.logger import Logger
+from csvRow.csvRow import CsvRow
 
 DELIMITER = ','
 
@@ -10,6 +11,20 @@ class InputReader():
     def __init__(self):
         self.logger = Logger()
 
+    def parseRow(self, row):
+        rowObj = CsvRow()
+        try:
+            rowObj.id = row['PtID']
+            rowObj.east = row['East']
+            rowObj.north = row['North']
+            rowObj.height = row['Height']
+            rowObj.code = row['Code']
+            rowObj.identifier = row['Identifier']
+        except:
+            raise KeyError
+
+        return rowObj
+
     def createCsvRowList(self, inpFileName):
         with io.open(inpFileName) as impFile:
             rows = []
@@ -17,16 +32,21 @@ class InputReader():
             reader = csv.DictReader(impFile, delimiter=DELIMITER)
             
             for row in reader:
-                rows.append(row)
-
+                try:
+                    rowObj = self.parseRow(row)
+                    rows.append(rowObj)
+                except KeyError:
+                    self.logger.error("Could not find all columns needed to process the file.")
+                    break
+                
             return rows
         
     def createDropDownList(self, rows):
         codes = []
 
         for obj in rows:
-            if obj['Code'] not in codes:
-                codes.append(obj['Code'])
+            if obj.code not in codes:
+                codes.append(obj.code)
 
         self.logger.info(f"Found {len(codes)} unique codes: {codes}")
 
@@ -36,7 +56,7 @@ class InputReader():
         list = []
 
         for obj in rows:
-            if obj['Code'] == code:
+            if obj.code == code:
                 list.append(obj)
 
         self.logger.info(f"Filtered by code {code}.")
@@ -47,7 +67,7 @@ class InputReader():
         identifiers = []
 
         for list in lists:
-            identifier = list[0]['Attribut1']
+            identifier = list[0].identifier
 
             if identifier not in identifiers:
                 identifiers.append(identifier)
@@ -64,7 +84,7 @@ class InputReader():
 
         for identifier in identifiers:
             for item in splitList:
-                if identifier == item[0]['Attribut1']:
+                if identifier == item[0].identifier:
                     geometryWrapper.append(item)
             geometryList.append(geometryWrapper)
             geometryWrapper = []        
@@ -82,13 +102,13 @@ class InputReader():
         list = []
         lists.append(list)
         
-        identifier = rows[0]['Attribut1']
+        identifier = rows[0].identifier
 
         for row in rows:
-            if row['Attribut1'] == identifier:
+            if row.identifier == identifier:
                 list.append(row)
             else:
-                identifier = row['Attribut1']
+                identifier = row.identifier
                 list = []
                 list.append(row)
                 lists.append(list)
